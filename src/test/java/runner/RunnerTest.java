f701aeaed3893ba06194470d36e75387d50292da
Carlos R <cruiza08@hotmail.com>
2025-07-05 21:19:58 -0500

    modify readme

--- src/test/java/runner/RunnerTest.java
+++ src/test/java/runner/RunnerTest.java
@@ -1,39 +1,43 @@
 package runner;
 
-import com.intuit.karate.junit5.Karate;
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
 import net.masterthought.cucumber.Configuration;
 import net.masterthought.cucumber.ReportBuilder;
 import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RunnerTest {
-    @Karate.Test
+ /*   @Karate.Test
     Karate testRunner() {
         return Karate.run("classpath:features")
                 .tags("@reqres").outputCucumberJson(true);
         //  .relativeTo(getClass());
-    }
+    }*/
 
-   /* @Test
+    @Test
     void testParallel() {
         Results results = Runner.path("classpath:features")
                 .outputCucumberJson(true)
                 .tags("@reqres")
-                //.karateEnv("demo")
+                .karateEnv("cert")
                 .parallel(3);
         generateReport(results.getReportDir());
         assertEquals(0, results.getFailCount(), results.getErrorMessages());
-    }*/
+    }
 
     public static void generateReport(String karateOutputPath) {
         Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[]{"json"}, true);
         List<String> jsonPaths = new ArrayList<>(jsonFiles.size());
         jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
-        Configuration config = new Configuration(new File("target"), "Taller Karate QS");
+        Configuration config = new Configuration(new File("target"), "Reqres Karate");
         ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
         reportBuilder.generateReports();
     }
